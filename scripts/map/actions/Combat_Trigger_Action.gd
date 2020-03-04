0e5fb6b811b3aea660d8dde091d00107cf5d827e
marc <marc.castells.guell@gmail.com>
2020-03-04 18:57:09 +0100

    Added tresure action and parsed it into engine

--- scripts/map/actions/Combat_Trigger_Action.gd
+++ scripts/map/actions/Combat_Trigger_Action.gd
@@ -3,8 +3,8 @@ extends Generic_Action
 class_name Combat_Trigger_Action
 
 func execute() -> void:
-	
 	var current_scene : Node = $"/root/Game_Manager".current_scene
 	if current_scene.name == "Map":
 		current_scene.combat_viewport.start_encounter(self.data)
 	self.queue_free()
+	emit_signal("finished")
---
+++ scripts/map/actions/Treasure_Action.gd
@@ -0,0 +1,8 @@
+extends Generic_Action
+
+class_name  Treasure_Action
+
+func execute() -> void:
+	# Here show the treasure UI & we add items and money to inventory
+	print("Treasure not yet implemented")
+	emit_signal("finished")
